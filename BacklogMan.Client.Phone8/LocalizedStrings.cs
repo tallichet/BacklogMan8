adaee4b8d2d1133080a314d3964beb0052ec57e0
Cédric Tallichet <kridek@gmail.com>
2013-07-10 21:24:24 +0200

    Add the Windows Phone 8 project

---
+++ BacklogMan.Client.Phone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BacklogMan.Client.Phone8.Resources;
+
+namespace BacklogMan.Client.Phone8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
