ab80b17a448860a6c6d205692b71e466d92ee670
Cedric Tallichet <kridek@gmail.com>
2013-07-24 00:19:22 +0200

    working on story look and feel

---
+++ BacklogMan.Client.App.Win8/Converters/StatusToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BacklogMan.Client.App.Win8.Converters
+{
+    public class StatusToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Core.Model.StoryStatus)
+            {
+                switch ((Core.Model.StoryStatus)value)
+                {
+                    case Core.Model.StoryStatus.InProgress:
+                        return "In Progress";
+                    case Core.Model.StoryStatus.ToDo:
+                        return "To Do";
+                    default:
+                        return ((Core.Model.StoryStatus)value).ToString();
+                }
+            }
+            return "unknown";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
