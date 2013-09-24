3d75a18fd4da5f5acee9fcaa6e811dadd0ab39cd
Cédric Tallichet <kridek@gmail.com>
2013-09-24 12:43:10 +0200

    Add converters from 8.0 project

---
+++ BacklogMan.Client.App.Win81/Converters/EmailToGravatarUriConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace BacklogMan.Client.App.Win81.Converters
+{
+    public class EmailToGravatarUriConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var email = value as string;
+
+            if (targetType == typeof(Uri))
+            {
+                return getGravatarUri(email);
+            }
+            else if (targetType == typeof(ImageSource))
+            {
+                return new BitmapImage(getGravatarUri(email));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Uri getGravatarUri(string email)
+        {
+
+            var hash = Md5(email.Trim().ToLower()).ToLower();
+
+            return new Uri("http://www.gravatar.com/avatar/" + hash);
+        }
+
+        private static string Md5(string source)
+        {
+            // Create a string that contains the name of the hashing algorithm to use.
+            String strAlgName = HashAlgorithmNames.Md5;
+
+            // Create a HashAlgorithmProvider object.
+            HashAlgorithmProvider objAlgProv = HashAlgorithmProvider.OpenAlgorithm(strAlgName);
+
+            // Create a CryptographicHash object. This object can be reused to continually
+            // hash new messages.
+            CryptographicHash objHash = objAlgProv.CreateHash();
+
+            // Hash message 1.
+            IBuffer buffMsg1 = CryptographicBuffer.ConvertStringToBinary(source, BinaryStringEncoding.Utf8);
+            objHash.Append(buffMsg1);
+            IBuffer buffHash1 = objHash.GetValueAndReset();
+
+            // Convert the hashes to string values (for display);
+            return CryptographicBuffer.EncodeToHexString(buffHash1);
+            //return CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8, buffHash1);
+
+        }
+    }
+}
---
+++ BacklogMan.Client.App.Win81/Converters/StringColorToSolidColorBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace BacklogMan.Client.App.Win81.Converters
+{
+    public class StringColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string)
+            {
+                var colorString = value as string;
+
+                if (colorString.StartsWith("#"))
+                {
+                    if (colorString.Length == 9)
+                    {
+                        colorString.Substring(1, 2);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
